8c7e51fb3153c362653ed1e015f5f449d0a95c5c
Matt Miller <milhous.30@gmail.com>
2017-01-30 05:28:20 +0000

    updating template

--- ArmCode/asm/template/lib.asm
+++ ArmCode/asm/template/lib.asm
@@ -3,7 +3,8 @@
 printd : .asciz "%d\n" 
 .balign 4
 stringd: .asciz "%s"
-
+.balign 4
+stringRegs: .asciz "Dump #%08X\nR0=%08X  R1=%08X  R2=%08X  R3=%08X  R4=%08X  R5=%08X  R6=%08X\nR7=%08X  R8=%08X  R9=%08X R10=%08X R11=%08X R12=%08X\nSP=%08X LR=%08X\n"
 .text
 .global print_int
 print_int:
@@ -19,3 +20,53 @@ print_string:
 	ldr r0, =stringd
 	bl printf	
 	pop {r0,r1,fp, pc}
+/*^^^^^^^^^^^^^^^^DUMP_REGS^^^^^^^^^^^^^^^^*/
+.global dump_regs
+.macro dump_regs, val
+	push {lr}
+	push {r1}
+	push {r1}
+	mov r1, #\val
+	str r1, [sp,#4]
+	pop {r1}
+	push {lr}	
+	bl dump_regs_sub
+	add sp, sp, #4
+	pop {lr}
+.endm
+.global dump_regs_sub
+dump_regs_sub:
+	push {lr}
+	push {r0-r12}
+	ldr r3, [sp, #4] 	
+	ldr r2, [sp, #0] 
+	ldr r1, [sp, #60]
+	/*lr*/	
+	ldr r0, [sp, #64]
+	push {r0}
+	/*sp*/
+	mov r0, sp
+	add r0, #64
+	push {r0}
+	push {r12}
+	push {r11}
+	push {r10}
+	push {r9}
+	push {r8}
+	push {r7}
+	push {r6}
+	push {r5}
+	push {r4}
+	ldr r0, [sp, #56]
+	push {r0} /*r3*/
+	ldr r0, [sp, #56]
+	push {r0} /*r2*/
+	ldr r0, =stringRegs
+	bl printf
+	add sp, #52
+	pop {r0-r12}
+	pop {lr}
+	bx lr
+
+	
+	
